fe37cf5ecd8f940c1a8e82803ec6b6c5f15724c7
Eddie Cunningham <edcunnin@umich.edu>
2018-01-14 19:07:53 -0600

    added support for subtraction in log space

--- pyLogVar.py
+++ pyLogVar.py
@@ -3,83 +3,158 @@ import numpy as np
 ZERO = 'ZERO'
 UNDERFLOW = 500
 
-def logAdd( log_a, log_b ):
+def _logAdd( logA, logB ):
+    # compute log( A + B ) using log( A ) and log( B )
+    return logA + np.log1p( np.exp( logB - logA ) )
 
-    if( log_a == ZERO ):
-        return log_b
-    if( log_b == ZERO ):
-        return log_a
+def _logSub( logA, logB ):
+    # compute log( A - B ) using log( A ) and log( B )
+    return logA + np.log1p( -np.exp( logB - logA ) )
 
-    if( log_a - log_b > UNDERFLOW ):
-        return log_a
-    elif( log_b - log_a > UNDERFLOW ):
-        return log_b
+def logAdd( logA, sgnA, logB, sgnB ):
+    if( logA == ZERO ):
+        return ( logB, sgnB )
+    if( logB == ZERO ):
+        return ( logA, sgnA )
 
-    if( log_b < log_a ):
-        return log_a + np.log1p( np.exp( log_b - log_a ) )
+    if( logA - logB > UNDERFLOW ):
+        # B is wayyyy smaller than A and is basically 0
+        return ( logA, sgnA )
+    elif( logB - logA > UNDERFLOW ):
+        # A is wayyyy smaller than B and is basically 0
+        return ( logB, sgnB )
+
+    if( sgnA == sgnB ):
+        # either computing ( a + b ) or -( a + b )
+
+        if( logB < logA ):
+            # make sure that the # that goes into np.exp isn't too big
+            return _logAdd( logA, logB ), sgnA
+        else:
+            return _logAdd( logB, logA ), sgnA
+
     else:
-        return log_b + np.log1p( np.exp( log_a - log_b ) )
+        # either computing ( a - b ) or -( a - b )
+        if( sgnA == 1 ):
+            # computing a - b
+            if( logB > logA ):
+                return ( _logSub( logB, logA ), -1 )
+            else:
+                return ( _logSub( logA, logB ), 1 )
+        else:
+            # computing b - a
+            if( logB < logA ):
+                return ( _logSub( logB, logA ), -1 )
+            else:
+                return ( _logSub( logA, logB ), 1 )
 
-def logMul( log_a, log_b ):
-    if( log_a == ZERO or log_b == ZERO ):
-        return ZERO
-    return log_a + log_b
+def logMul( logA, sgnA, logB, sgnB ):
+    if( logA == ZERO or logB == ZERO ):
+        return ( ZERO, 1 )
+    if( sgnA == sgnB ):
+        return ( logA + logB, 1 )
+    else:
+        return ( logA + logB, -1 )
 
-def logDiv( log_a, log_b ):
-    if( log_a == ZERO ):
-        return ZERO
-    if( log_b == ZERO ):
+def logDiv( logA, sgnA, logB, sgnB ):
+    if( logA == ZERO ):
+        return ( ZERO, 1 )
+    if( logB == ZERO ):
         assert 0
-    return log_a - log_b
+    if( sgnA == sgnB ):
+        return ( logA - logB, 1 )
+    else:
+        return ( logA - logB, -1 )
 
-def extractVal( val ):
-    if( 'LogVar' in str( type( val ) ) or 'instance' in str( type( val ) ) ):
-        return val.logVal
+def extractVal( val, sgn=None ):
+
+    if( isinstance( val, LogVar ) ):
+        return ( val.logVal, val.sgn )
+
     if( val == 0 ):
-        return ZERO
-    return np.log( val )
+        return ( ZERO, 1 )
+
+    if( sgn is not None ):
+        return ( np.log( val ), sgn )
+
+    if( val > 0 ):
+        return ( np.log( val ), 1 )
+
+    return ( np.log( val ), -1 )
 
 class LogVar():
-    def __init__( self, val=None, isLog=False ):
+    def __init__( self, val=None, sgn=None, isLog=False ):
         if( val == None ):
             self.logVal = ZERO
+            self.sgn = 1
         elif( isLog ):
             self.logVal = val
+            if( sgn is not None ):
+                self.sgn = sgn
+            else:
+                self.sgn = 1
         else:
-            self.logVal = extractVal( val )
+            self.logVal, self.sgn = extractVal( val, sgn )
+
+    def genericOperator( self, other, func, otherNeg=False ):
+        otherVal, sgn = extractVal( other )
+        if( otherNeg ):
+            sgn *= -1
+        val, valSgn = func( self.logVal, self.sgn, otherVal, sgn )
+        return LogVar( val=val, sgn=valSgn, isLog=True )
+
+    def iGenericOperator( self, other, func, otherNeg=False ):
+        otherVal, sgn = extractVal( other )
+        if( otherNeg ):
+            sgn *= -1
+        self.logVal, self.sgn = func( self.logVal, self.sgn, otherVal, sgn )
+        return self
+
+    """ Addition """
 
     def __add__( self, other ):
-        otherVal = extractVal( other )
-        return LogVar( logAdd( self.logVal, otherVal ), isLog=True )
+        return self.genericOperator( other, logAdd )
 
     def __radd__( self, other ):
         return self+other
 
     def __iadd__( self, other ):
-        otherVal = extractVal( other )
-        self.logVal = logAdd( self.logVal, otherVal )
-        return self
+        return self.iGenericOperator( other, logAdd )
+
+    """ Subtraction """
+
+    def __sub__( self, other ):
+        return self.genericOperator( other, logAdd, otherNeg=True )
+
+    def __rsub__( self, other ):
+        return self+other
+
+    def __isub__( self, other ):
+        return self.iGenericOperator( other, logAdd, otherNeg=True )
+
+    """ Multiplication """
 
     def __mul__( self, other ):
-        otherVal = extractVal( other )
-        return LogVar( logMul( self.logVal, otherVal ), isLog=True )
+        return self.genericOperator( other, logMul )
 
     def __rmul__( self, other ):
         return self*other
 
     def __imul__( self, other ):
-        otherVal = extractVal( other )
-        self.logVal = logMul( self.logVal, otherVal )
-        return self
+        return self.iGenericOperator( other, logMul )
+
+    """ Division """
 
     def __truediv__( self, other ):
-        otherVal = extractVal( other )
-        return LogVar( logDiv( self.logVal, otherVal ), isLog=True )
+        return self.genericOperator( other, logDiv )
 
     def __idiv__( self, other ):
-        otherVal = extractVal( other )
-        self.logVal = logDiv( self.logVal, otherVal )
-        return self
+        return self.iGenericOperator( other, logDiv )
+
+    """ Power """
+
+    def __pow__( self, exponent ):
+        return LogVar( val=self.logVal * float( exponent ), isLog=True )
 
     def toFloat( self ):
         if( self.logVal == ZERO ):
@@ -92,7 +167,7 @@ class LogVar():
     def __float__( self ):
         if( self.logVal == ZERO ):
             return 0.
-        return np.exp( self.logVal )
+        return self.sgn * np.exp( self.logVal )
 
     def __int__( self ):
         return int( float( self ) )
@@ -109,3 +184,14 @@ def test():
     for i in range( 100000 ):
         val1 += val2
         print( val1.logVal )
+
+# a = LogVar( 0 )
+# a += 4
+# a -= 2
+# a *= 1.3
+# a /= 59
+# a += 3
+# a -= 1.2
+# b = LogVar( 0.12 )
+# a -= b
+# print( a )
